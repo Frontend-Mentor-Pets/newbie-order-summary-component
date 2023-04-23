e95c8ceb1882e67533da20bebccd9116cb0157e0
Bumble <bumble.sakh@gmail.com>
2023-04-24 07:01:04 +1100

    feat: add classes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,25 +5,27 @@ import music from '../../assets/images/icon-music.svg';
 const Card = () => {
   return (
     <div className={styles.card}>
-      <div>
+      <div className={styles.hero}>
         <img src={hero} alt='Hero' />
       </div>
-      <h1>Order Summary</h1>
-      <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
-      <div>
-        <div>
+      <h1 className={styles.title}>Order Summary</h1>
+      <p className={styles.subtitle}>
+        You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!
+      </p>
+      <div className={styles.payment}>
+        <div className={styles.icon}>
           <img src={music} alt='Music' />
         </div>
-        <div>
-          <span> Annual Plan</span>
-          <span>$59.99/year</span>
+        <div className={styles.plan}>
+          <span className={styles.name}> Annual Plan</span>
+          <span className={styles.price}>$59.99/year</span>
         </div>
-        <div>
+        <div className={styles.change}>
           <span>Change</span>
         </div>
       </div>
-      <button>Proceed to Payment</button>
-      <button>Cancel Order</button>
+      <button className={styles.proceed}>Proceed to Payment</button>
+      <button className={styles.cancel}>Cancel Order</button>
     </div>
   );
 };
